7ccbb52bd1624232ffa38b906d1f5d942d4f00ea
MicroOptimization <jackiezhen538@gmail.com>
2023-01-27 15:13:45 -0500

    profile format pretty much done, next steps are the actual content, linking it to my homepage (or making this my homepage) or mobile optimizations

---
+++ static/scripts.js
@@ -0,0 +1,7 @@
+function showEmail() {
+    if (document.getElementById("email_label").style.visibility == "visible") {
+        document.getElementById("email_label").style.visibility = "hidden"
+    } else {
+        document.getElementById("email_label").style.visibility = "visible"
+    }
+}
